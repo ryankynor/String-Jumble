4bb795831f4c8958935c452d3f2fa2cb3a37f8c6
ryankynor <ryankynor@hanovernorwichschools.org>
2015-10-15 14:47:59 -0400

    Updated from Brython Server: 10/15/2015 2:48:01 PM

--- stringjumble.py
+++ stringjumble.py
@@ -27,6 +27,11 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 text = input("Please enter a string of text (the bigger the better): ")
 
+
 print ('You entered "{0}".'.format(text))
+
 print (text [::-1])
 print (" ".join(text.split()[::-1]))
+print (" ".join(text.split())[::])
+
+
